c05d06acac7327dab3a77ad3d13407de9b44f39a
fayeza khatun <fayezaaktarjoya@gmail.com>
2026-05-17 21:31:25 +0600

    country commited

--- react-world-on-the-app/src/assets/components/Countries/Country/Country.jsx
+++ react-world-on-the-app/src/assets/components/Countries/Country/Country.jsx
@@ -4,6 +4,7 @@ const Country = ({country}) => {
     console.log(country.flags.flags.png)
     return (
         <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
         </div>
     );
